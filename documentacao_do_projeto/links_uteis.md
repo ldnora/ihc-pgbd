84fbaa779c465bd83d4057e52697ad9a3a07f3d3
Leandro Dalla Nora <workdallanora@gmail.com>
2024-12-04 18:34:08 -0300

    Adição de plugins para o calendário

--- documentacao_do_projeto/links_uteis.md
+++ documentacao_do_projeto/links_uteis.md
@@ -3,4 +3,5 @@
 - [O que é uma API](https://developer.mozilla.org/pt-BR/docs/Learn/Server-side/Express_Nodejs/Introduction)
 - [Sobre o node.js e express.js](https://developer.mozilla.org/pt-BR/docs/Learn/Server-side/Express_Nodejs/Introduction)
 - [Mini projeto para aprender a criar uma API CRUD com node.js, express.js e sqlite](https://www.youtube.com/watch?v=yEpiT-N2DUc&list=PLygIEirBzJi4lTC-5nzfhEyxuKq2y1uiR)
+- [Documentação do Schedule-x](https://schedule-x.dev/)
 - 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useCalendarApp, ScheduleXCalendar } from '@schedule-x/react'
 import {
-    createCalendar,
     createViewDay,
     createViewMonthAgenda,
     createViewMonthGrid,
@@ -9,39 +8,65 @@ import {
 } from '@schedule-x/calendar'
 import { createEventsServicePlugin } from '@schedule-x/events-service'
 import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop'
+import { createResizePlugin } from '@schedule-x/resize'
+import { createCurrentTimePlugin } from '@schedule-x/current-time'
+import { createEventModalPlugin } from '@schedule-x/event-modal'
 
 import '@schedule-x/theme-default/dist/index.css'
 
 function CalendarApp() {
-    const eventsService = useState(() => createEventsServicePlugin())[0]
+    const [events, setEvents] = useState([
+        {
+            id: '1',
+            title: 'Evento 1',
+            start: '2024-12-03 04:00',
+            end: '2024-12-03 05:00',
+        },
+    ]);
+
+    const eventsService = useState(() => createEventsServicePlugin())[0];
 
     const calendar = useCalendarApp({
-        views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
-        events: [
-            {
-                id: '1',
-                title: 'Event 1',
-                start: '2024-12-03 04:00',
-                end: '2024-12-03 05:00',
-            },
+        locale: 'pt-BR',
+        isDark: true,
+        firstDayOfWeek: 0,
+        views: [
+            createViewDay(),
+            createViewWeek(),
+            createViewMonthGrid(),
+            createViewMonthAgenda(),
         ],
+        events: events,
         plugins: [
             eventsService,
-            createDragAndDropPlugin()
+            createDragAndDropPlugin(),
+            createResizePlugin(),
+            createCurrentTimePlugin(),
+            createEventModalPlugin(),
         ],
-        isDark: true
-    })
+    });
 
     useEffect(() => {
-        // get all events
-        eventsService.getAll()
-    }, [])
+        eventsService.getAll();
+    }, [eventsService]);
+
+    const handleDateSelect = ({ start, end }) => {
+        const title = prompt('Digite o título do evento:');
+        if (title) {
+            const newEvent = { id: `${events.length + 1}`, title, start, end };
+            setEvents((prevEvents) => [...prevEvents, newEvent]);
+        }
+    };
 
     return (
         <div>
-            <ScheduleXCalendar calendarApp={calendar} />
+            <ScheduleXCalendar
+                calendarApp={calendar}
+                onDateRangeSelect={handleDateSelect} // Listener para clique e arraste
+                selectable
+            />
         </div>
-    )
+    );
 }
 
-export default CalendarApp
+export default CalendarApp;
